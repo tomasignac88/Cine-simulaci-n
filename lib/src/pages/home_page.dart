5ea2318757932feb9d8c66a2000007cbee6c1718
Tomas Masiero <tmasiero@sowit.com.ar>
2020-07-26 21:00:21 -0300

    subo version cine simulacion proyecto viejo en mis comienzos con flutter

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -4,72 +4,65 @@ import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class HomePage extends StatelessWidget {
-
   final peliculasProvider = new PeliculasProvider();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Peliculas de cine'),
+          title: Center(child: Text('Cine simulación')),
           backgroundColor: Colors.indigoAccent,
-          actions: <Widget>[
-            IconButton(icon: Icon(Icons.search),
-            onPressed: (){},
-            ),
-          ],
+          // actions: <Widget>[
+          //   IconButton(
+          //     icon: Icon(Icons.search),
+          //     onPressed: () {},
+          //   ),
+          // ],
         ),
         body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              _swiperTarjetas(),
-              _footer(context)
-            ],
-          )
-        )
-    );
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[_swiperTarjetas(), _footer(context)],
+        )));
   }
 
   Widget _swiperTarjetas() {
-
     return FutureBuilder(
-      future: peliculasProvider.getEnCines(),
-      builder: (BuildContext context, AsyncSnapshot<List> snapshot){
-        if(snapshot.hasData){
-          return CardSwiper(peliculas: snapshot.data);
-        }else{
-          return Container(
-              child: Center(
-                child: CircularProgressIndicator()
-              )
-            );
-        }
-      }
-    );
+        future: peliculasProvider.getEnCines(),
+        builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+          if (snapshot.hasData) {
+            return CardSwiper(peliculas: snapshot.data);
+          } else {
+            return Container(child: Center(child: CircularProgressIndicator()));
+          }
+        });
   }
 
   Widget _footer(BuildContext context) {
-
     return Container(
       width: double.infinity,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            padding: EdgeInsets.only(left:20.0),
-            child: Text("Populares",style: Theme.of(context).textTheme.subhead)
-          ),
+              padding: EdgeInsets.only(left: 20.0),
+              child: Center(
+                child: FittedBox(
+                  child: Text("Populares",
+                      style: Theme.of(context).textTheme.subhead),
+                ),
+              )),
           FutureBuilder(
             future: peliculasProvider.getPopulares(),
-            builder: (BuildContext context, AsyncSnapshot<List> snapshot){
-              if(snapshot.hasData){
-                return MovieHorizontal(peliculas: snapshot.data);
-              }else{
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+              if (snapshot.hasData) {
+                return Container(
+                    child: MovieHorizontal(peliculas: snapshot.data));
+              } else {
                 return Center(child: CircularProgressIndicator());
               }
             },
-            )
+          )
         ],
       ),
     );
--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -1,50 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
 
-
 class MovieHorizontal extends StatelessWidget {
-
   final List<Pelicula> peliculas;
 
   const MovieHorizontal({@required this.peliculas});
 
   @override
   Widget build(BuildContext context) {
-
     final _screenSize = MediaQuery.of(context).size;
     return Container(
       height: _screenSize.height * 0.2,
       child: PageView(
         pageSnapping: false,
-        controller: PageController(
-          initialPage: 1,
-          viewportFraction: 0.3
-        ),
+        controller: PageController(initialPage: 1, viewportFraction: 0.3),
         children: _tarjetas(context),
       ),
     );
   }
 
   List<Widget> _tarjetas(BuildContext context) {
-    return peliculas.map( (pelicula) {
+    return peliculas.map((pelicula) {
       return Container(
         margin: EdgeInsets.only(right: 15.0),
         child: Column(
           children: <Widget>[
             ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),//redondear imagenes
+              borderRadius: BorderRadius.circular(20.0), //redondear imagenes
               child: FadeInImage(
                 placeholder: AssetImage('assets/img/no-image.jpg'),
-                image: NetworkImage( pelicula.getPosterImg()),
+                image: NetworkImage(pelicula.getPosterImg()),
                 fit: BoxFit.cover,
-                height: 160.0,
+                height: 140.00,
               ),
             ),
-            SizedBox(height: 5.0),
-            Text(
-              pelicula.title,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.caption,
+            FittedBox(
+              child: Text(
+                pelicula.title,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.caption,
+              ),
             )
           ],
         ),
